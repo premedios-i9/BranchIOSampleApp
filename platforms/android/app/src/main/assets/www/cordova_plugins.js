50a735b57a1a5561db2479ccfa4116239f0a0b35
Pedro Remedios <premedios@innovagency.com>
2021-04-28 07:36:07 +0100

    - Test changes

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,6 +1,16 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
+  module.exports = [
+    {
+      "id": "cordova-plugin-branchio.plugin",
+      "file": "plugins/cordova-plugin-branchio/www/branchioplugin.js",
+      "pluginId": "cordova-plugin-branchio",
+      "clobbers": [
+        "cordova.plugins.branchio"
+      ]
+    }
+  ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-branchio": "1.0.0"
   };
 });
