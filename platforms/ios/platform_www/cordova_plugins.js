93cce8e81330dfdc6d2ccf3ec3087edf014b7fd6
Pedro Remedios <premedios@innovagency.com>
2021-05-04 10:06:55 +0100

    - Added ios platform that was being ignored

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "branch-cordova-sdk.Branch",
+      "file": "plugins/branch-cordova-sdk/src/index.js",
+      "pluginId": "branch-cordova-sdk",
+      "clobbers": [
+        "Branch"
+      ]
+    },
+    {
+      "id": "cordova-plugin-branchio.plugin",
+      "file": "plugins/cordova-plugin-branchio/www/branchioplugin.js",
+      "pluginId": "cordova-plugin-branchio",
+      "clobbers": [
+        "cordova.plugins.branchio"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "branch-cordova-sdk": "4.2.4",
+    "cordova-plugin-branchio": "1.0.0",
+    "cordova-plugin-whitelist": "1.3.4"
+  };
+});
